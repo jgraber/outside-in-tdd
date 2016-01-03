a5924481721917665e6a4deffc09017aee3836e6
Johnny Graber <jg@jgraber.ch>
2016-01-03 18:38:40 +0100

    add more helper code

---
+++ WalkingSkeleton/RunningJournalApi.AcceptanceTests/JsonContent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace RunningJournalApi.AcceptanceTests
+{
+    public class JsonContent : StringContent
+    {
+        public JsonContent(object value)
+            : base(SerializeToJson(value))
+        {
+        }
+
+        private static string SerializeToJson(object value)
+        {
+            return JsonConvert.SerializeObject(value);
+        }
+    }
+}
