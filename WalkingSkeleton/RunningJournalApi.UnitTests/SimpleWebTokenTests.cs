61e6ce061cba065a37755862c6c914cec47d42b9
Johnny Graber <jg@jgraber.ch>
2016-01-03 21:11:28 +0100

    use tests to drive out SimpleWebToken

---
+++ WalkingSkeleton/RunningJournalApi.UnitTests/SimpleWebTokenTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RunningJournalApi.UnitTests
+{
+    public class SimpleWebTokenTests
+    {
+        [Fact]
+        public void SutIsIteratorOfClaims()
+        {
+            var sut = new SimpleWebToken();
+            Assert.IsAssignableFrom<IEnumerable<Claim>>(sut);
+        }
+
+        [Fact]
+        public void SutYieldInjectedClaims()
+        {
+            var expected = new[]
+            {
+                new Claim("foo", "bar"),
+                new Claim("baz", "qux"),
+                new Claim("quux", "corge")
+            };
+
+            var sut = new SimpleWebToken(expected);
+
+            Assert.True(expected.SequenceEqual(sut));
+            Assert.True(expected.Cast<object>().SequenceEqual(((System.Collections.IEnumerable)sut).OfType<object>()));
+        }
+    }
+}
---
+++ WalkingSkeleton/RunningJournalApi/SimpleWebToken.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace RunningJournalApi
+{
+    public class SimpleWebToken : IEnumerable<Claim>
+    {
+        private readonly IEnumerable<Claim> claims;
+        public SimpleWebToken(params Claim[] claims)
+        {
+            this.claims = claims;
+        }
+
+        public IEnumerator<Claim> GetEnumerator()
+        {
+            return this.claims.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
