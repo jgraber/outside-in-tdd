cc3fa6286f8d6ad4920dfd1791184fb9b475e296
Johnny Graber <jg@jgraber.ch>
2016-01-03 18:28:07 +0100

    add class ContentReader for ReadAsJsonAsync method

---
+++ WalkingSkeleton/RunningJournalApi.AcceptanceTests/ContentReader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Xml.Linq;
+
+namespace RunningJournalApi.AcceptanceTests
+{
+    /// <summary>
+    /// Code from https://github.com/ploeh/RunningJournalApi/blob/master/RunningJournalApi.AcceptanceTests/ContentReader.cs
+    /// </summary>
+    public static class ContentReader
+    {
+        public static Task<dynamic> ReadAsJsonAsync(this HttpContent content)
+        {
+            if (content == null)
+                throw new ArgumentNullException("content");
+
+            return content.ReadAsStringAsync().ContinueWith(t =>
+                JsonConvert.DeserializeObject(t.Result));
+        }
+
+        public static Task<XDocument> ReadAsXmlAsync(this HttpContent content)
+        {
+            if (content == null)
+                throw new ArgumentNullException("content");
+
+            return content.ReadAsStringAsync().ContinueWith(t =>
+                XDocument.Parse(t.Result));
+        }
+    }
+}
