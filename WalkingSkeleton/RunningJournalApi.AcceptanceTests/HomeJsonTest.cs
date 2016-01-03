6415b6718ca067f3e1fabdf13f37898cd16508ec
Johnny Graber <jg@jgraber.ch>
2016-01-03 18:12:43 +0100

    extract helper code to own class

--- WalkingSkeleton/RunningJournalApi.AcceptanceTests/HomeJsonTest.cs
+++ WalkingSkeleton/RunningJournalApi.AcceptanceTests/HomeJsonTest.cs
@@ -15,13 +15,8 @@ namespace RunningJournalApi.AcceptanceTests
         [Fact]
         public void GetResponseReturnCorrectStatusCode()
         {
-            var baseAddress = new Uri("http://localhost:9876");
-            var config = new HttpSelfHostConfiguration(baseAddress);
-            new Bootstrap().Configure(config);
-            var server = new HttpSelfHostServer(config);
-            using (var client = new HttpClient(server))
+            using (var client = HttpClientFactory.Create())
             {
-                client.BaseAddress = baseAddress;
                 var response = client.GetAsync("").Result;
 
                 Assert.True(
