3cb4b9f6d6bc9b2678cb4001174cca1103e2bcfa
Johnny Graber <jg@jgraber.ch>
2016-01-03 23:50:48 +0100

    copy code to JournalEntriesQuery

--- WalkingSkeleton/RunningJournalApi/IJournalEntriesQuery.cs
+++ WalkingSkeleton/RunningJournalApi/IJournalEntriesQuery.cs
@@ -1,6 +1,9 @@
-﻿namespace RunningJournalApi
+﻿using System.Collections.Generic;
+
+namespace RunningJournalApi
 {
     public interface IJournalEntriesQuery
     {
+        IEnumerable<JournalEntryModel> GetJournalEntries(string userName);
     }
 }
---
+++ WalkingSkeleton/RunningJournalApi/JournalEntriesQuery.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunningJournalApi
+{
+    class JournalEntriesQuery : IJournalEntriesQuery
+    {
+        private readonly dynamic db;
+
+        public JournalEntriesQuery(dynamic db)
+        {
+            this.db = db;
+        }
+        public IEnumerable<JournalEntryModel> GetJournalEntries(string userName)
+        {
+            var entries = this.db.JournalEntry
+                .FindAll(db.JournalEntry.User.UserName == userName)
+                .ToArray<JournalEntryModel>();
+            return entries;
+        }
+    }
+}
