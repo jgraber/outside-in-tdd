d4bbdb6a05a53220724a80a118df7f6cfd2da9d4
Johnny Graber <jg@jgraber.ch>
2016-01-03 18:02:00 +0100

    move AcceptanceTest project into a folder with the same name

---
+++ WalkingSkeleton/RunningJournalApi.AcceptanceTests/HomeJsonTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.SelfHost;
+using Xunit;
+using RunningJournalApi;
+
+namespace RunningJournalApi.AcceptanceTests
+{
+    public class HomeJsonTest
+    {
+        [Fact]
+        public void GetResponseReturnCorrectStatusCode()
+        {
+            var baseAddress = new Uri("http://localhost:9876");
+            var config = new HttpSelfHostConfiguration(baseAddress);
+            new Bootstrap().Configure(config);
+            var server = new HttpSelfHostServer(config);
+            using (var client = new HttpClient(server))
+            {
+                client.BaseAddress = baseAddress;
+                var response = client.GetAsync("").Result;
+
+                Assert.True(
+                    response.IsSuccessStatusCode,
+                    "Actual status code: " + response.StatusCode);
+            }
+        }
+    }
+}
--- WalkingSkeleton/RunningJournalApi.AcceptanceTests/Properties/AssemblyInfo.cs
+++ WalkingSkeleton/RunningJournalApi.AcceptanceTests/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("96e802e6-fcdb-4ff6-8846-75858d1ab6e5")]
+[assembly: Guid("ce138bb1-e1c9-46c4-9a28-28ea62056c7d")]
 
 // Version information for an assembly consists of the following four values:
 //
