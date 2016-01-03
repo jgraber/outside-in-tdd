fc571366fe1ad57010c89b2d9b906164f17f9794
Johnny Graber <jg@jgraber.ch>
2016-01-03 23:55:29 +0100

    copy code to AddJournalEntryCommand

---
+++ WalkingSkeleton/RunningJournalApi/AddJournalEntryCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunningJournalApi
+{
+    class AddJournalEntryCommand : IAddJournalEntryCommand
+    {
+        private readonly dynamic db;
+
+        public AddJournalEntryCommand(dynamic db)
+        {
+            this.db = db;
+        }
+
+        public void AddJournalEntry(JournalEntryModel journal, string userName)
+        {
+            var userId = this.db.User
+                .FindAllByUserName(userName)
+                .Select(this.db.User.UserId)
+                .ToScalarOrDefault<int>();
+
+            if (userId == 0)
+            {
+                userId = this.db.User.Insert(UserName: userName).UserId;
+            }
+
+            this.db.JournalEntry.Insert(
+                UserId: userId,
+                Time: journal.Time,
+                Distance: journal.Distance,
+                Duration: journal.Duration);
+        }
+    }
+}
--- WalkingSkeleton/RunningJournalApi/IAddJournalEntryCommand.cs
+++ WalkingSkeleton/RunningJournalApi/IAddJournalEntryCommand.cs
@@ -2,5 +2,6 @@
 {
     public interface IAddJournalEntryCommand
     {
+        void AddJournalEntry(JournalEntryModel journal, string userName);
     }
 }
