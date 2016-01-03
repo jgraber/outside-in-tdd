8f35c5f06f6bc76c3a7c989e1da4c40e3e76552f
Johnny Graber <jg@jgraber.ch>
2016-01-04 00:21:25 +0100

    add unit test for Controller

---
+++ WalkingSkeleton/RunningJournalApi.UnitTests/JournalControllerTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Hosting;
+using Xunit;
+using Moq;
+
+namespace RunningJournalApi.UnitTests
+{
+    public class JournalControllerTests
+    {
+        [Fact]
+        public void GetReturnsCorrectResult()
+        {
+            var queryStub = new Mock<IJournalEntriesQuery>();
+            var cmdDummy = new Mock<IAddJournalEntryCommand>();
+            var sut = new JournalController(queryStub.Object, cmdDummy.Object)
+            {
+                Request = new HttpRequestMessage()
+            };
+            sut.Request.Properties.Add(
+                HttpPropertyKeys.HttpConfigurationKey,
+                new HttpConfiguration());
+            sut.Request.Headers.Authorization = 
+                new System.Net.Http.Headers.AuthenticationHeaderValue(
+                    "Bearer",
+                    new SimpleWebToken(new Claim("userName", "foo")).ToString());
+
+            var expected = new[]
+            {
+                new JournalEntryModel
+                {
+                    Time = new DateTime(2016, 1, 4),
+                    Distance = 5780,
+                    Duration = TimeSpan.FromMinutes(33)
+                },
+                new JournalEntryModel
+                {
+                    Time = new DateTime(2016, 1, 3),
+                    Distance = 5760,
+                    Duration = TimeSpan.FromMinutes(31)
+                },
+                new JournalEntryModel
+                {
+                    Time = new DateTime(2016, 1, 2),
+                    Distance = 5700,
+                    Duration = TimeSpan.FromMinutes(30)
+                }
+            };
+
+            queryStub.Setup(q => q.GetJournalEntries("foo")).Returns(expected);
+
+            var response = sut.Get();
+            var actual = response.Content.ReadAsAsync<JournalModel>().Result;
+
+            Assert.True(expected.SequenceEqual(actual.Entries));
+        }
+    }
+}
