772bff2ddf1a363ef7eca1e8075d9a9dfb8e08ed
Johnny Graber <jg@jgraber.ch>
2016-01-04 21:53:44 +0100

    add characterization test for SimpleWebTokenUserNameProjection

---
+++ WalkingSkeleton/RunningJournalApi.UnitTests/SimpleWebTokenUserNameProjectionTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RunningJournalApi.UnitTests
+{
+    public class SimpleWebTokenUserNameProjectionTests
+    {
+        [Fact]
+        public void GetUserNameFromProperSimpleWebTokenReturnsCorrectResult()
+        {
+            var sut = new SimpleWebTokenUserNameProjection();
+
+            var request = new HttpRequestMessage();
+            request.Headers.Authorization = 
+                new AuthenticationHeaderValue(
+                    "Bearer",
+                    new SimpleWebToken(new Claim("userName", "foo")).ToString());
+
+            var actual = sut.GetUserName(request);
+
+            Assert.Equal("foo", actual);
+        }
+    }
+}
